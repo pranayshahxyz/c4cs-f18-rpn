1256e77a706f04ebd81e833f97079e092b84f826
pranays <pranays@umich.edu>
2018-10-26 15:23:24 -0400

    Implemented and tested multiplication and division. Handled too many arguments.

--- rpn.py
+++ rpn.py
@@ -8,21 +8,31 @@ def calculate(arg):
 		try:
 			stack.append(int(token))
 		except ValueError:
-			val1 = stack.pop()
 			val2 = stack.pop()
+			val1 = stack.pop()
 			if token == '+':
 				result = val1 + val2
 			elif token == '-':
-				result = val2 - val1
+				result = val1 - val2
+			elif token == '*':
+				result = val1 * val2
+			elif token == '/':
+				result = val1 / val2
 
 			stack.append(result)
+
+	if len(stack) > 1:
+			raise ValueError('Too many arguments on the stack')
 
 	return stack[0]
 
 def main():
 	while True:
-		result = calculate(input('rpn calc> '))
-		print(result)
+		try:
+			result = calculate(input('rpn calc> '))
+			print(result)
+		except ValueError:
+			pass
 
 if __name__ == '__main__':
 	main()
